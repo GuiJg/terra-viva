19c9d364b02466d612dabdf25428cb019a682f2b
GuiJg <j.gu1lh3rm325@gmail.com>
2023-12-19 18:33:44 -0300

    feat: responsive layout n menu button script

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,6 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector(".header");
+const menuButton = document.querySelector('input');
+const headerMenu = document.querySelector('.header-menu');
   
-    if (this.window.pageYOffset > 0){
-      header.classList.add("sticky");
-    }
-  
-    else{
-      header.classList.remove("sticky");
-    }
+menuButton.addEventListener('click', function() {
+    headerMenu.classList.toggle('active');
 });
-
-window.addEventListener("scroll", function(){
-    let header = document.querySelector(".header-menu");
-  
-    if (this.window.pageYOffset > 0){
-      header.classList.add("sticky");
-    }
-  
-    else{
-      header.classList.remove("sticky");
-    }
-});
-
-// let count = 1;
-// document.getElementById("radio1").checked = true;
-
-// setInterval(() => {
-//   nextImage()
-// }, 5000);
-
-// function nextImage(){
-//   count++;
-//   if(count > 3){
-//     count = 1;
-//   }
-
-//   document.getElementById("radio"+count).checked = true;
-// }
